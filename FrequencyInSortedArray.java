274b45f76d36d2b57b98e68c5684861e700c5230
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2025-09-10 23:29:19 +0530

    Add files via upload

---
+++ FrequencyInSortedArray.java
@@ -0,0 +1,27 @@
+public class FrequencyInSortedArray {
+    public static void countFrequency(int[] arr){
+        int n=arr.length;
+        int i=1;
+        int freq=1;
+        while(i<n){
+            while(i<n && arr[i]==arr[i-1]){
+                freq++;
+                i++;
+            }
+            System.out.println(arr[i-1]+" "+freq);
+            i++;
+            freq=1;
+        }
+        if(n==1 || arr[n-1]!=arr[n-2]){
+            System.out.println(arr[n-1]+" "+freq);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={10,10,10,20,20,30};//
+        int[] arr1={10,10,10,10};
+        int[] arr2={10,20,30};
+        countFrequency(arr);
+        countFrequency(arr1);
+        countFrequency(arr2);
+    }
+}
